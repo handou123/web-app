9625946751d11f476b5ad3ca27587c216e0c422e
handou123 <liqian9323@163.com>
2017-12-19 19:32:22 +0800

    web

---
+++ js/fontSize.js
@@ -0,0 +1,7 @@
+function pgScale(){
+    var deviceWidth = document.documentElement.clientWidth;
+    if(deviceWidth > 750) deviceWidth = 750;
+    document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
+}
+pgScale();
+window.onresize=pgScale;
